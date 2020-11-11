505617ed094fee761e6b76a002a48657e82ef0db
Swami Gallardo Aquino <swamikaas@gmail.com>
2020-11-11 22:44:37 +0100

    embeddable table comprasProducto

---
+++ src/main/java/com/swamigallardo/market/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,20 @@
+package com.swamigallardo.market.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Embeddable
+public class ComprasProductoPK implements Serializable {
+
+    @Column(name = "id_compra")
+    private Long idCompra;
+
+    @Column(name = "id_producto")
+    private Long idProducto;
+}

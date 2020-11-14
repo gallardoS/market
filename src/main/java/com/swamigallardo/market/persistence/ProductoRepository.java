9f46c54840e82beecca0b12464f96d32dfd1673a
Swami Gallardo Aquino <swamikaas@gmail.com>
2020-11-14 16:01:01 +0100

    producto repository

---
+++ src/main/java/com/swamigallardo/market/persistence/ProductoRepository.java
@@ -0,0 +1,39 @@
+package com.swamigallardo.market.persistence;
+
+import com.swamigallardo.market.persistence.crud.ProductoCrudRepository;
+import com.swamigallardo.market.persistence.entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll(){
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(Long idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(Long idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(Long idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+}
---
+++ src/main/java/com/swamigallardo/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,14 @@
+package com.swamigallardo.market.persistence.crud;
+
+import com.swamigallardo.market.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Long>{
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(Long idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
